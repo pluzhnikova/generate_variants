e87540838dd775a223ec7cc58753f9a1283c643b
pluzhnikova <58787450+pluzhnikova@users.noreply.github.com>
2023-02-18 13:51:05 +0300

    Update tasks_create.sql

--- tasks_create.sql
+++ tasks_create.sql
@@ -1,20 +1,20 @@
-CREATE TABLE parameters(parameters_id int, template_id int, parametr varchar(1000)[], task varchar(1000), REZULT varchar(1000)[]);
-CREATE TABLE template(template_id int, text varchar(1000), query varchar(1000), number_of_parameters int, level int);
-ALTER TABLE parameters ADD CONSTRAINT parameters_id_pk PRIMARY KEY (parameters_id);
+CREATE TABLE template(template_id int, template_text varchar(1000), template_query varchar(1000), number_of_parameters int, level int);
+CREATE TABLE variant(variant_id int, template_id int, parameters varchar(1000)[], variant_text varchar(1000), result varchar(1000)[]);
+ALTER TABLE variant ADD CONSTRAINT variant_id_pk PRIMARY KEY (variant_id);
 ALTER TABLE template ADD CONSTRAINT template_id_pk PRIMARY KEY (template_id);
-ALTER TABLE parameters ADD FOREIGN KEY (template_id) REFERENCES template;
+ALTER TABLE variant ADD FOREIGN KEY (template_id) REFERENCES template;
 
 CREATE SEQUENCE template_seq
 START WITH 1
 INCREMENT BY 1;		
 					  
-CREATE SEQUENCE parameters_seq
+CREATE SEQUENCE variant_seq
 START WITH 1
 INCREMENT BY 1;
 
 /*
 drop sequence template_seq;
-drop sequence parameters_seq;
+drop sequence variant_seq;
 drop table template;	
-drop table parameters;
+drop table variant;
 */
